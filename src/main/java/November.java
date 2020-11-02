59d2f6696a49f9918a4097804ac1877ea021afa1
arhmcolak92 <arahimcolak01@hotmail.com>
2020-11-01 20:28:26 -0600

    I added some class

---
+++ src/main/java/November.java
@@ -0,0 +1,8 @@
+public class November {
+
+    public static void main(String[] args) {
+        System.out.println("Today is a good day");
+        System.out.println("Hi");
+        System.out.println("HEllo");
+    }
+}
